c5081006deb9b686a233e39641e9a212090c9b93
Kushagra-Varshney <kushagra.ashishvarshney@gmail.com>
2024-07-07 13:33:45 +0530

    Added routes for Messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,106 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "You must be Authenticated to accept messages"
+            },
+            { status: 401 }
+        );
+    }
+
+    const userId = user._id;
+    const {acceptMessages} = await request.json();
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(userId, {isAcceptingMessage: acceptMessages}, {new: true});
+        if(!updatedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Failed to change the message accpetance status",
+                    updatedUser
+                },
+                { status: 401 }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Message accpetance status changed successfully"
+            },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        console.log("Failed to change the message accpetance status");
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to change the message accpetance status"
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "You must be Authenticated to accept messages"
+            },
+            { status: 401 }
+        );
+    }
+
+    const userId = user._id;
+
+    try {
+        const user = await UserModel.findById(userId);
+        if(!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                { status: 404 }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "User found",
+                isAccpetingMessages: user.isAcceptingMessage
+            },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        console.log("Failed to get the user");
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to get the user"
+            },
+            { status: 500 }
+        );
+    }
+}
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,59 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { Message } from "@/models/User";
+
+
+export async function POST(request: Request) {
+    await dbConnect();
+    //since anyone can send message the user need not be logged in to maintain anonymity
+
+    const { username, content } = await request.json();
+
+    try {
+        const user = await UserModel.findOne({
+            username
+        });
+
+        if(!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                { status: 404 }
+            );
+        }
+        //check if user is even accepting the messages
+        if(!user.isAcceptingMessage) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Messages for this user are currently disabled"
+                },
+                { status: 403 }
+            );
+        }
+
+        const newMessage = {content, createdAt: new Date()};
+        user.messages.push(newMessage as Message);
+        await user.save();
+
+        return Response.json(
+            {
+                success: true,
+                message: "Message sent successfully"
+            },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        console.error("Failed to send message");
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to send message"
+            },
+            { status: 500 }
+        );
+    }
+}
